5ecf7997921536b0088a67e9abdf41374f4090ee
Neha Upadhyay <upaneha1212@gmail.com>
2019-03-26 23:55:48 +0530

    Adding project for Shelter queue

---
+++ QueueOperations/QueueUsingStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueOperations
+{
+    public class QueueUsingStack
+    {
+        Stack s1 = new Stack();
+        Stack s2 = new Stack();
+
+        public static void Runner()
+        {
+            QueueUsingStack obj = new QueueUsingStack();
+            obj.Dequeue();
+            obj.Enqueue(10);
+            obj.Enqueue(20);
+            obj.Enqueue(30);
+            obj.Enqueue(40);
+            obj.Dequeue();
+            obj.Dequeue();
+            obj.Enqueue(50);
+            obj.Enqueue(60);
+            obj.Enqueue(70);
+            obj.Dequeue();
+            obj.Dequeue();
+
+        }
+        public void Enqueue(int data)
+        {
+            s1.Push(data);
+            Console.WriteLine($"Element entered in queue is {data}");
+        }
+
+        public void Dequeue()
+        {
+            if (s1.Count == 0 && s2.Count == 0)
+                Console.WriteLine("Queue is empty");
+            else
+            {
+                if (s2.Count == 0)
+                {
+                    while (s1.Count != 0)
+                    {
+                        s2.Push(s1.Pop());
+                    }
+
+                }
+                int temp = (int)s2.Pop();
+                Console.WriteLine($"Element removed from queue is {temp}");
+            }
+        }
+    }
+}
---
+++ ShelterQueue/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShelterQueue
+{
+    class Animal
+    {
+        public string name;
+        public string typeOfAnimal;
+        public Animal(string name, string typeOfAnimal)
+        {
+            this.name = name;
+            this.typeOfAnimal = typeOfAnimal;
+        }
+    }
+
+    class AnimalShelter
+    {
+        LinkedList<Animal> animals;
+
+        public AnimalShelter()
+        {
+            animals = new LinkedList<Animal>();
+        }
+
+        public void Enqueue(Animal animal)
+        {
+            animals.AddLast(animal);
+        }
+
+        public Animal DequeueAny()
+        {
+            Animal animal = animals.First.Value;
+            animals.RemoveFirst();
+            return animal;
+        }
+
+        public Animal DequeueDog()
+        {
+            Animal retVal=null;
+            foreach (var item in animals)
+            {
+                if(item.typeOfAnimal == "Dog")
+                {
+                    retVal = item;
+                    animals.Remove(item);
+                    break;
+                }
+            }
+            return retVal;
+        }
+
+        public Animal DequeueCat()
+        {
+            Animal retVal = null;
+            foreach (var item in animals)
+            {
+                if (item.typeOfAnimal == "Cat")
+                {
+                    retVal = item;
+                    animals.Remove(item);
+                    break;
+                }
+            }
+            return retVal;
+        }
+
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            AnimalShelter obj = new AnimalShelter();
+            obj.Enqueue(new Animal("Cat1", "Cat"));
+            obj.Enqueue(new Animal("Dog1", "Dog"));
+            obj.Enqueue(new Animal("Cat2", "Cat"));
+            obj.Enqueue(new Animal("Dog2", "Dog"));
+            obj.Enqueue(new Animal("Cat3", "Cat"));
+            obj.Enqueue(new Animal("Cat4", "Cat"));
+            obj.Enqueue(new Animal("Dog3", "Dog"));
+            obj.Enqueue(new Animal("Cat5", "Cat"));
+
+            Animal anyAnimal = obj.DequeueAny();
+            Console.WriteLine($"You are adopting a {anyAnimal.typeOfAnimal} named {anyAnimal.name}");
+
+            Animal animalCat = obj.DequeueCat();
+            Console.WriteLine($"You are adopting a Cat named {animalCat.name}");
+
+            Animal anyAnimal1 = obj.DequeueAny();
+            Console.WriteLine($"You are adopting a {anyAnimal1.typeOfAnimal} named {anyAnimal1.name}");
+
+            obj.Enqueue(new Animal("Dog4", "Dog"));
+
+            Animal anyAnimal2 = obj.DequeueAny();
+            Console.WriteLine($"You are adopting a {anyAnimal2.typeOfAnimal} named {anyAnimal2.name}");
+
+            Animal animalDog = obj.DequeueDog();
+            Console.WriteLine($"You are adopting a Dog named {animalDog.name}");
+
+            Animal animalDog1 = obj.DequeueDog();
+            Console.WriteLine($"You are adopting a Dog named {animalDog1.name}");
+        }
+    }
+}
